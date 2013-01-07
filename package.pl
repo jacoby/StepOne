72d21a2c32b45a5f568829091aa2f1f5ab00c22f
Dave Jacoby <jacoby.david@gmail.com>
2013-01-07 12:51:52 -0500

    cleaned up package

--- package.pl
+++ package.pl
@@ -8,24 +8,20 @@ use Data::Dumper ;
 use lib '.' ;
 use StepOne ':all' ;
 
-my $sample_names = [
-    'sample 1' ,
-    'sample 2' ,
-    'sample 4' ,
-    ] ;
+my $sample_names = [ 'sample 1', 'sample 2', 'sample 4', ] ;
 my $temp = '/tmp/eds' ;
 
 my $plate ;
-$plate ->{ experiment_name } = 'Plate #34' ;   # name of experiment
-$plate->{ sample_names }     = $sample_names ; # list of samples
-$plate ->{ replicates }      = 3 ;    # list of replicates per sample
-$plate ->{ std_curve }       = 4 ; # highest num in standard curve
-my $eds = make_eds( $plate , $temp ) ;
+$plate->{ experiment_name } = 'Plate #34' ;      # name of experiment
+$plate->{ sample_names }    = $sample_names ;    # list of samples
+$plate->{ replicates } = 3 ;    # list of replicates per sample
+$plate->{ std_curve }  = 4 ;    # highest num in standard curve
+my $eds = make_eds( $plate, $temp ) ;
 
 #from here, write $eds to a file or to STDOUT
 
-if ( open my $fh , '>' , '/home/jacoby/foo.eds' ) {
-    print $fh $eds ;    
+if ( open my $fh, '>', '/home/jacoby/foo.eds' ) {
+    print $fh $eds ;
     }
 else {
     say STDERR $! ;
